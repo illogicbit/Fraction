e9d575a999d987e3acf3454ea0a6d001dbcdddcc
Recatsu <79374155+RexBeGoinLike@users.noreply.github.com>
2024-02-17 11:35:24 +0800

    Updated GCD Computation and Fraction Reduction

--- FractionArithmetic.java
+++ FractionArithmetic.java
@@ -7,9 +7,9 @@ public class FractionArithmetic {
       SecondDenominator = remainder; //Assign the value of the remainder to the second denominator
     }while(remainder != 0);
     return FirstDenominator; //Display the previous remainder before reaching 0
-  }  
+    }  
 
-  public void reduce(int numerator, int denominator){
-     System.out.printf("result is %d/%d or %.1f.", numerator, denominator, ((double) numerator/denominator));
-  }
+    public String reduce(int numerator, int denominator){
+        return String.format("%d/%d", numerator/computeGCD(numerator, denominator), denominator/computeGCD(numerator, denominator));
+    }
 }
