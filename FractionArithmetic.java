3f16834ecb99793d3f3d89bec007b59804982aa3
Recatsu <79374155+RexBeGoinLike@users.noreply.github.com>
2024-02-17 10:27:18 +0800

    Update FractionArithmetic.java
    For computeGCD() method and reduce() method

--- FractionArithmetic.java
+++ FractionArithmetic.java
@@ -1,2 +1,16 @@
 public class FractionArithmetic {
+
+  public int computeGCD(int FirstDenominator, int SecondDenominator){
+    int remainder = 0;
+    do{
+      remainder = FirstDenominator % SecondDenominator;
+      FirstDenominator = SecondDenominator;
+      SecondDenominator = remainder;
+    }while(remainder != 0);
+    return FirstDenominator;
+  }  
+
+  public void reduce(int numerator, int denominator){
+     System.out.printf("result is %d/%d or %.1f.", numerator, denominator, ((double) numerator/denominator));
+  }
 }
